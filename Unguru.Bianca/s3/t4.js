1be3ec052906053fbacbbb4693b525d03924a691
Hixyan <bug.bianca@gmail.com>
2019-04-09 23:49:01 +0300

    Tema

---
+++ Unguru.Bianca/s3/t4.js
@@ -0,0 +1,5 @@
+var Tema4=function(a,b) {
+    if(a<b) {
+    console.log(a)}
+    if(b<a) {
+    console.log(b)}}
---
+++ Unguru.Bianca/s3/t6.js
@@ -0,0 +1,9 @@
+var Tema6=function(N){
+    var a=0;
+    for (i=2; i<N;i++){
+    if (N%i==0)
+    a=1}
+    if (a==0) {
+    console.log("Numar Prim")}
+    else { 
+    console.log("Nu e numar prim")}}
