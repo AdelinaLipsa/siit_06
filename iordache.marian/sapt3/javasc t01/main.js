ede96993e02fd2a2809ca7c28eef96e4f878453c
iordache_marian <pusa_ak@yahoo.com>
2019-04-12 23:27:04 +0300

    Tema Sapt3 Functii

--- iordache.marian/sapt3/javasc t01/main.js
+++ iordache.marian/sapt3/javasc t01/main.js
@@ -130,6 +130,35 @@ function primFunction(N){
 
     */
 
+
+
+    /* 7 part 2
+     function primFunction(N){
+    var sum = 0;                // initializare suma
+    var count = 0;              // contorizeaza numere prime
+    var n = 1;                  // variabila pentru test prim
+    while (count < N) {         // cat timp nu am gasit N numere prime
+        prim = 0;               // contorizeaza divizori
+        n = n + 1;              // incrementare n pentru a parcurge valorile pana la n
+        for (i=1; i<=n; i++)    
+        if(n % i == 0) {        // test numere prime
+            prim = prim + 1;       // incrementam divizorii
+            }
+        if (prim == 2) {           // daca se gasesc 2 divizori numarul este prim
+            sum = sum + n;          // se aduca numarul prim n
+            count = count + 1;      // se contorizeaza numerele prime
+            }
+            
+     }
+             
+    
+    return sum;                     // retuneaza suma
+}
+
+    console.log(primFunction(4)); 
+
+    */
+
 /*  8
  inversString = function(str) {
     var invers = '';
@@ -169,7 +198,105 @@ console.log(inversString('abc'));
 
     
 
+ /* 10
+    arr = [1, 5, 8, 10];
+     x = 8;
+    findNumber = function(arr, x) {
+      for (i=0; i<=arr.length -1; i++) {  // se parcurge vectorul
+          if (x == arr[i]) {                // verifica pentru fiecare element al vectorului daca este egal cu x
+              return true;
+          }
+      }
+
+  } 
+  console.log(findNumber(arr,x));   
+  */
+
+  /* 11
+    arr = [2, 9, 4, 5, 3, 1 ,6];
+    findMaxim = function(arr) {
+        max = arr[0];
+        for(i=1; i<=arr.length - 1; i++){
+            if (arr[i]>max) 
+            max = arr[i];
+            
+        } 
+        return max;
+    }
+console.log(findMaxim(arr));
+*/
+
+/*  12
+arr = [2,7,8,9];
+sumMinMax = function(arr) {
+    sum = 0;            
+    max = arr[0];   // initializez max cu valoarea primei pozitii a arr
+    min = arr[0];       // initializez min cu valoarea primei pozitii a arr
+    for (i=1; i<=arr.length - 1; i++) {   // parcurgem arr
+        if (arr[i]>max) {
+            max = arr[i];
+        }
+        if (arr[i]<min) {
+            min = arr[i];
+        }     
+    }
+    sum = min + max;
+    return sum;
+
+}
+console.log(sumMinMax(arr));
+*/
+
+ /* 13
+
+  arr = [2,5,2,6,8,3];
+
+  findDublicates = function(arr) {
+      for (i=0; i<=arr.length - 1; i++) {  // parcurgem vectorul
+        for (j=i+1; j<=arr.length -1; j++) {  // parcurgem vectorul cu pozitia + 1
+              if (arr[i] == arr[j]) {
+              return true;
+          } 
+      }
+   }
+}
+  console.log(findDublicates(arr));
+  */
 
 
 
+   /* 14
+
+  arr = [1, -3, 3, 7,-9, 4];
+
+  produsNumere = function (arr) {
+      prod = 1;
+      for (i=0; i<=arr.length -1; i++) {
+          if (arr[i] > 0)
+          prod = prod * arr[i];
+          
+
+      }
+      return prod;
+  }
+  
+  console.log(produsNumere(arr));
+  */
+
+/*
+  15 
+  palindrom = function (str) {
+      invers = ''; 
+    for(i=str.length - 1; i>=0; i--) {
+        invers = invers + str[i];
+    }
+    if (str==invers ){
+        return true;
+  } else {
+        return false;
+  }
+}
+  console.log(palindrom(str='abcba'));
+  console.log(palindrom(str='pcccbb'));
+  */
 
