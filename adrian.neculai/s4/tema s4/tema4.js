d220f9ef1c8c09fcc61465feb6da5554ccd789b7
adrianneculai <adrian.razvan83@gmail.com>
2019-04-15 13:35:02 +0300

    pending...

--- adrian.neculai/s4/tema s4/tema4.js
+++ adrian.neculai/s4/tema s4/tema4.js
@@ -1,8 +1,10 @@
 var a = ["a", "b", "c", 1, 2, 3, "d", "e", "f", 4, 5, 6];
 var b = [[1, 2, 3, 4], ["a", "b", "c", "d"], [5, 6, 7, 8], ["e", "f", "g", "h"]];
 var nr = 5;
+var sort = [22, 13, 14, 6, 9, 3, 8, 5, 12];
 var divA = 12;
-var divB = 42;
+var divB = 1;
+var pal = 14141;
 
 
 console.log(a);
@@ -149,7 +151,6 @@ function celMaiMareDiv(p, n) {
                 cmmdc = sirP[p];
                 break;
             }
-
         }
     }
     console.log("cmmdc este: " + cmmdc);
@@ -163,3 +164,143 @@ console.log(" al doilea numar este: " + divB);
 console.log("");
 celMaiMareDiv(divA, divB);
 console.log("");
+
+function celMaiMicMultiplu(p, n) {
+    var s = 0;
+    var sir = [];
+    var cmmmmc = 0;
+    s = p * n;
+    for (var i = s; i > 0; i--) {
+        if (i % p == 0 && i % n == 0) {
+            sir.push(i);
+        }
+
+    }
+
+    console.log("cmmmc este: " + sir[sir.length - 1]);
+
+}
+
+console.log("9. Calculeaza cel mai mic multiplu comun al 2 numere");
+console.log("");
+console.log(" primul numar este: " + divA);
+console.log(" al doilea numar este: " + divB);
+console.log("");
+celMaiMicMultiplu(divA, divB);
+console.log("");
+
+
+function divizori(p) {
+    var sir = [];
+
+    for (var i = 2; i < p; i++) {
+        if (p % i == 0) {
+            sir.push(i);
+        }
+
+    }
+    if (sir.length != 0) {
+        console.log("divizorii sunt: " + sir);
+    } else {
+
+        console.log("Este numar prim!!!")
+    }
+}
+
+console.log("10. Returneaza un array care sa contina toti divizorii unui numar (ex pentru 64: trebuie sa returneze [2,4,8,16,32])");
+console.log("");
+console.log(" Numarul este: " + divA);
+console.log("");
+divizori(divA);
+console.log("");
+
+
+function chackPali(p) {
+    toChar = "" + p + "";
+    var suma = 0;
+    for (var i = 0; i <= toChar.length - 1; i++) {
+        /*      console.log(str.substr(i, 1));
+              console.log(str.substr(str.length - i - 1, 1));  */
+        if (toChar.substr(i, 1) != toChar.substr(toChar.length - i - 1, 1)) {
+            gage = true;
+        } else {
+            gage = false;
+        }
+    }
+    if (gage == true) {
+        console.log("numarul NU este palindrom");
+    } else {
+        console.log("numarul este palindrom");
+    }
+}
+console.log("11. O functie care verifica daca un numar este palindrom (ex: 121, 1234321)");
+console.log("");
+console.log(" Numarul este: " + pal);
+console.log("");
+chackPali(pal);
+console.log("");
+
+function sortarePar(p) {
+    var sirPar = [];
+    for (i = 0; i < p.length; i++) {
+        if (p[i] % 2 == 0) {
+            sirPar.push(p[i]);
+        }
+    }
+    console.log(" Sirul de numere pare este: " + sirPar);
+}
+console.log("12. O functie care sorteaza numerele pare dintr-un sir de numere primit ca parametru.");
+console.log("");
+console.log("Sirul de numere este: " + sort);
+console.log("");
+sortarePar(sort);
+console.log("");
+
+
+function sorAscSir(p) {
+
+
+    for (i = 0; i < p.length; i++) {
+
+
+
+    }
+
+}
+
+
+
+function sortAscParDescImp(p) {
+    var sirComplet = [];
+    var sirImpar = [];
+    var sirPar = [];
+    for (i = 0; i < p.length; i++) {
+        if (p[i] % 2 == 0) {
+            sirPar.push(p[i]);
+        }
+    }
+    console.log(" Sirul de numere pare este: " + sirPar);
+
+    for (i = p.length - 1; i > 0; i--) {
+        if (p[i] % 2 != 0) {
+            sirImpar.push(p[i]);
+        }
+    }
+    console.log(" Sirul de numere impare este: " + sirImpar);
+
+    sirComplet.push(sirPar);
+    sirComplet.push(sirImpar);
+    concatenareListaSiruri(sirComplet);
+
+
+
+}
+console.log("13. functie care primeste ca parametru un array de numere. Aceasta sorteaza ascendent numerele pare si descendent numerele impare, in cadrul aceluiasi array primit ca parameru.");
+console.log("");
+console.log("Sirul de numere este: " + sort);
+console.log("");
+sortAscParDescImp(sort);
+console.log("");
+
+
+
