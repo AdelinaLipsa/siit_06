f46a618e62391c86725f15ee09350130f8b701fb
andrei-caj <and.cajvan@gmail.com>
2019-04-19 20:05:47 +0300

    tema s3

--- cajvan.andrei/s3/tema.js
+++ cajvan.andrei/s3/tema.js
@@ -82,6 +82,31 @@ function nrprim1() {
         }
     }
 }
+/*
+function nrprim2() {
+    var x = document.getElementById("nrN4");
+    var n = x.value;
+    var p = parseInt(n);
+    var sum = 1;
+    if (p === 1) {
+        rezultat9.innerHTML = "Numarul nu este prim.";
+    }
+    else if (p === 2) {
+        rezultat9.innerHTML = "Numarul este prim.";
+    }
+    for (var i = 1; i <= p; i++) {
+        for (var j = 2; j <= 1000;j++) {
+            
+            sum = sum + sum;
+            break;
+        }
+        else {
+            rezultat9.innerHTML = "error";
+        }
+        rezultat9.innerHTML = sum;
+    }
+}*/
+//"@%#$$@%^%#"
 
 function textinvert() {
     var x = document.getElementById("text1");
@@ -103,5 +128,99 @@ function produs1() {
         }
     }
     rezultat8.innerHTML = p;
+}
+
+function arrsearch1() {
+    var x = document.getElementById("nrN5");
+    var n = x.value;
+    var p = parseInt(n);
+    var y = document.getElementById("arr1");
+    var z = y.value;
+    var arr = z.split(", ");
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == p) {
+            rezultat10.innerHTML = "L-am gasit!";
+            break;
+        }
+        else {
+            rezultat10.innerHTML = "Nu e :(";
+        }
+    }
+}
+
+function arrsearch2() {
+    var x = document.getElementById("arr2");
+    var n = x.value;
+    var arr = n.split(", ");
+    var p = 0;
+    for (i = 0; i < arr.length; i++) {
+        var int = parseInt(arr[i]);
+        if (int > p) {
+            p = int;
+        }
+        rezultat11.innerHTML = p;
+    }
+}
+
+function arrsearch3() {
+    var x = document.getElementById("arr3");
+    var n = x.value;
+    var arr = n.split(", ");
+    var p = 0;
+    var q = arr[0];
+    for (i = 0; i < arr.length; i++) {
+        var max = parseInt(arr[i]);
+        if (max > p) {
+            p = max;
+        }}
+    for (i = 0; i < arr.length; i++) {
+        var min = parseInt(arr[i]);
+        if(min < q)    
+        q = min;
+        }
+        rezultat12.innerHTML = p + q;
+}
+
+function arrsearch4() {
+    var x = document.getElementById("arr4");
+    var n = x.value;
+    var arr = n.split(", ");
+    var sorted = arr.sort();
+    for (i = 0; i < sorted.length-1; i++) {
+        if (sorted[i+1] == sorted[i]) {
+            rezultat13.innerHTML = "Are.";
+            break;
+        }
+        else {
+            rezultat13.innerHTML = "Nu are.";
+        }
+    }
+}
+
+function arrsearch5() {
+    var x = document.getElementById("arr5");
+    var n = x.value;
+    var arr = n.split(", ");
+    var p = 1;
+    for (i = 0; i < arr.length; i++) {
+        var int = parseInt(arr[i]);
+        if (int >= 0) {
+            p = p*int;
+        }
+        }
+        rezultat14.innerHTML = p;
+}
+
+function strsearch1() {
+    var x = document.getElementById("arr5");
+    var n = x.value;
+    var arr = n.split("");
+    var rev = arr.reverse();
+        if (arr != rev) {
+            rezultat15.innerHTML = "E palindrom.";
+        }
+        else {
+            rezultat15.innerHTML = "Nu e palindrom.";
+        }
 }
 
