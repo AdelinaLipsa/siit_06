93e47a91709171b04728a623a1d81809e966712e
ciocalucian <cioca.lucian@gmail.com>
2019-04-20 05:08:00 +0300

    t4 finish

--- cioca.lucian/t4/script.js
+++ cioca.lucian/t4/script.js
@@ -141,3 +141,140 @@ function palindroM() {
 	}
 
 }
+
+function arrayCreate(t) {
+	
+    let txt = document.getElementById(t).value; 
+    let p = 0;
+    let np= 0;
+    let a = [];
+    let b = txt.length;
+
+    for (let i = 0; i < b; i++){
+        if ( txt[i] == ','|| txt[i]==' '){
+            np = i;
+            let t = txt.slice(p,np);
+            p = np+1;
+            a.push(Number(t));
+        } 
+    }
+    let x = txt.slice(p,b);
+    a.push(Number(x));
+    return a;
+
+}
+
+function primeNumbers() {
+	var x = arrayCreate('text12');
+	var prime = [];
+
+	for ( var i = 0; i < x.length; i++ ){
+		if ( x[i] % 2 == 0 ){
+			prime.push(x[i]);
+		}
+	}
+	document.getElementById('demo12').innerHTML = prime;
+}
+
+function arraySort (x) {
+	for ( var i = 0; i < x.length; i++ ) {
+		for ( var j = i+1; j < x.length; j++ ){
+			if ( x[i] % 2 != 0) {
+				var z = x[i];
+				x[i] = x[j];
+				x[j] = z;
+			}
+		}
+	}
+}
+
+function midIndex(x) {
+	var p = 0;
+	for ( i = 0; i < x.length; i++ ){
+		if ( x[i] % 2 == 0 ) {
+			p++;
+		} else {
+			break;
+		}
+	}
+	return p;
+}
+
+function numbersSort(x) {
+	var x = arrayCreate('text13');
+	console.log(x);
+	arraySort(x);
+	var p = midIndex(x);
+	console.log(p);
+	
+	for ( var i = 0; i < p; i++ ) {
+		for ( var j = i+1; j < p; j++ ){
+			if ( x[i] > x[j]) {
+				var z = x[i];
+				x[i] = x[j];
+				x[j] = z;
+			}
+		}
+	}
+
+	for ( var i = p; i < x.length; i++ ) {
+		for ( var j = i+1; j <x.length; j++ ){
+			if ( x[i] < x[j]) {
+				var z = x[i];
+				x[i] = x[j];
+				x[j] = z;
+			}
+		}
+	}
+	document.getElementById('demo13').innerHTML = x;
+}
+var binaryTimes = 0; 
+
+function binaryY(x, t) {
+	binaryTimes++;
+
+	if(x.length == 0) {
+		return false;
+    } else {
+		var indxMid = Math.floor(x.length / 2);
+		var mid = x[indxMid];		
+		if (mid == t) {
+			return true;
+        } else {
+			var arr_left = [...x].splice(0, indxMid);
+			var arr_right = [...x].splice(indxMid, x.length-1);
+        	if (t < mid) {
+				return binaryY(arr_left, t);
+            } else {
+				return binaryY(arr_right, t);
+            }
+		}
+    }
+}
+
+function searchNum() {
+	var x = arrayCreate('text14');
+	var t = document.getElementById('num14').value;
+	var z = binaryY(x,t);
+
+	if ( z == true){
+		document.getElementById('demo14').innerHTML = 'Numarul se afla in array';
+	} else {
+		document.getElementById('demo14').innerHTML = 'Numarul nu se afla in array';
+	}
+
+}
+
+function searchPoz() {
+	var x = arrayCreate('text15');
+	var t = document.getElementById('num15').value;
+	var z = binaryY(x,t);
+
+	if ( z == true){
+		document.getElementById('demo15').innerHTML = binaryTimes;
+		binaryTimes = 0;
+	} else {
+		document.getElementById('demo15').innerHTML = 'Numarul nu se afla in array';
+	}
+
+}
