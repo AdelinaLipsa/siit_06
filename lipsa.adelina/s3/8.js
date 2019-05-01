678bc860aff8da7777d0f15b073299aa005b3c7f
Lipsa Adelina <adelina.lipsa@gmail.com>
2019-05-01 22:38:54 +0300

    code cleanup in S3

--- lipsa.adelina/s3/8.js
+++
@@ -1,6 +0,0 @@
-// O functie care primeste un parametru de tip string si intoarce inversul acestuia (abc => cba)
-var str = "abc";
-function reverseString(str) {
-    return str.split('').reverse('').join('');
-}
-console.log(reverseString(str));
