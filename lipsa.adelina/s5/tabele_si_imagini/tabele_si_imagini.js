31fd97b0b36ca3649aec6cd6a8c405f167c31125
Lipsa Adelina <adelina.lipsa@gmail.com>
2019-05-25 23:20:49 +0300

    updated missing code

---
+++ lipsa.adelina/s5/tabele_si_imagini/tabele_si_imagini.js
@@ -0,0 +1,12 @@
+function schimbaImaginea(event,elem){
+
+    elem.src="http://www.drodd.com/images14/x12.jpg";
+
+}
+schimbaImaginea();
+
+function normalImg(event, elem) {
+    elem.src= "http://static.wixstatic.com/media/b77fe464cfc445da9003a5383a3e1acf.jpg";
+}
+normalImg();
+
